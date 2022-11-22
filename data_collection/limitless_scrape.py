67b88ace4749308dbe0748d8eaed7869c87979d5
Andrew Dang <andrew.dang94@gmail.com>
2022-11-22 16:45:30 -0500

    scrape and deploy

--- data_collection/limitless_scrape.py
+++ data_collection/limitless_scrape.py
@@ -357,7 +357,7 @@ def scrape_for_dates_and_url():
         df.loc[length] = row_data
 
     # filter tournaments for late nights; exclude special events 
-df_latenight = df[
+    df_latenight = df[
     (df[df.columns[1]].str.contains("Late Night #\d{2}")) | 
     (df[df.columns[1]].str.contains("Late Night Series #")) & 
     (~df[df.columns[1]].str.contains("Late Late")) |
